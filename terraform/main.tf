9b0d283a811cbf3e74f6e3b6ee5872c3ecfadc54
ss23-bit <ss23lonewolf@gmail.com>
2026-03-22 15:16:50 +0700

    chore: final fix, add grafana ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,35 +23,44 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_key_pair" "deployer" {
-    key_name = "devops-lab-key"
-    public_key = file("~/.ssh/id_ed25519.pub")
-  
+  key_name   = "devops-lab-key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+
 }
 
 resource "aws_security_group" "web_sg" {
-    name = "devops-lab-sg"
-    description = "Allow http and SSH traffic"
-    
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["58.8.249.98/32"] # IP Whitelisting
-    }
+  name        = "devops-lab-sg"
+  description = "Allow http and SSH traffic"
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["58.8.249.253/32"] # IP Whitelisting
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 # Create the EC2 instance
 resource "aws_instance" "app_server" {
@@ -59,7 +68,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t3.micro"
 
   # Attach the Key and Security Group
-  key_name = aws_key_pair.deployer.key_name
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   credit_specification {
