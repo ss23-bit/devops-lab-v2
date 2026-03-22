17ee3d658bef12776fb32fb59a0134a1bc8ce4f9
ss23-bit <ss23lonewolf@gmail.com>
2026-03-22 13:23:49 +0700

    feat: add terraform infrastructure and update compose for production

---
+++ terraform/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Fetch the latest Ubuntu 22.04 AMI (Amazon Machine Image)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+    key_name = "devops-lab-key"
+    public_key = file("~/.ssh/id_ed25519.pub")
+  
+}
+
+resource "aws_security_group" "web_sg" {
+    name = "devops-lab-sg"
+    description = "Allow http and SSH traffic"
+    
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["58.8.249.98/32"] # IP Whitelisting
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+# Create the EC2 instance
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  # Attach the Key and Security Group
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  # This script runs automatically when the server boots
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y docker.io docker-compose
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ubuntu 
+              EOF
+  # "sudo usermod -aG docker ubuntu" is to run Docker commands without typing sudo
+
+  # To avoid surprise credit charges
+  tags = {
+    Name        = "devops-lab-v2-server"
+    Environment = "production"
+  }
+
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+
+}
