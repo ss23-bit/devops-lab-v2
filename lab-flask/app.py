43aaa63cd571ca1fc60dcb096dc47cce305510c1
ss23-bit <ss23lonewolf@gmail.com>
2026-04-19 01:09:13 +0700

    chores: editing flask for testing gitaction

--- lab-flask/app.py
+++ lab-flask/app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/health')
 def health_check():
-    return "OK", 200
+    return "status: Healthy", 200
 
 if __name__ == ("__main__"):
     app.run(host='0.0.0.0', port=5000)
