b82d0a76ec59f8ae5b9eda5c94608d24cd80490f
ss23-bit <ss23lonewolf@gmail.com>
2026-04-20 00:12:44 +0700

    chore: Daily, flask app and Dockerfile

--- lab-flask/app.py
+++ lab-flask/app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import psutil
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return jsonify({"Hello": "goodbye"}), 200
 
 @app.route('/health')
 def health_check():
-    return "status: Healthy", 200
+    cpu_usage = psutil.cpu_percent()
+    mem_usage = psutil.virtual_memory().percent
+
+    status = "Healthy"
+    status_code = 200
+
+    if cpu_usage > 95:
+        status = "Unhealthy"
+        status_code = 503
+    
+    return jsonify ({
+        "status": status,
+        "cpu usage": cpu_usage,
+        "memory usage": mem_usage
+    }), status_code
 
 if __name__ == ("__main__"):
     app.run(host='0.0.0.0', port=5000)
+        
+
